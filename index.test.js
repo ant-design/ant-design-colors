faa8406636ef6fa57be58039f0476129ffcbabaa
afc163 <afc163@gmail.com>
2018-12-23 20:55:02 +0800

    :hammer: TypeScript!

--- index.test.js
+++ index.test.js
@@ -1,36 +1,36 @@
-import { generate, presetPalettes } from './src';
-
-const blueColors = [
-  '#E6F7FF',
-  '#BAE7FF',
-  '#91D5FF',
-  '#69C0FF',
-  '#40A9FF',
-  '#1890FF',
-  '#096DD9',
-  '#0050B3',
-  '#003A8C',
-  '#002766',
-].map(color => color.toLowerCase());
-
-test('Generate palettes from a given color', () => {
-  expect(generate('#1890ff')).toEqual(blueColors);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var src_1 = require("./src");
+var blueColors = [
+    '#E6F7FF',
+    '#BAE7FF',
+    '#91D5FF',
+    '#69C0FF',
+    '#40A9FF',
+    '#1890FF',
+    '#096DD9',
+    '#0050B3',
+    '#003A8C',
+    '#002766',
+].map(function (color) { return color.toLowerCase(); });
+test('Generate palettes from a given color', function () {
+    expect(src_1.generate('#1890ff')).toEqual(blueColors);
 });
-
-test('should contain preseted palettes', () => {
-  expect(Object.keys(presetPalettes)).toEqual([
-    'red',
-    'volcano',
-    'orange',
-    'gold',
-    'yellow',
-    'lime',
-    'green',
-    'cyan',
-    'blue',
-    'geekblue',
-    'purple',
-    'magenta',
-  ]);
-  expect(presetPalettes.blue).toEqual(blueColors);
+test('should contain preseted palettes', function () {
+    expect(Object.keys(src_1.presetPalettes)).toEqual([
+        'red',
+        'volcano',
+        'orange',
+        'gold',
+        'yellow',
+        'lime',
+        'green',
+        'cyan',
+        'blue',
+        'geekblue',
+        'purple',
+        'magenta',
+        'grey',
+    ]);
+    expect(src_1.presetPalettes.blue).toEqual(blueColors);
 });
