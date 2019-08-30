541b61a149977464ba58b8af3353f69cba3113e3
Henning Pohlmeyer <hpohlmeyer@users.noreply.github.com>
2019-08-30 08:16:30 +0200

    Expose isLight and isDark functions

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import { generate, presetPalettes } from '../src';
+import { generate, isLight, isDark, yellow, purple, presetPalettes } from '../src';
 
 export const blueColors = [
   '#E6F7FF',
@@ -15,6 +15,16 @@ export const blueColors = [
 
 test('Generate palettes from a given color', () => {
   expect(generate('#1890ff')).toEqual(blueColors);
+});
+
+test('Determine the lightness of a color', () => {
+  expect(isLight(yellow[5])).toEqual(true);
+  expect(isLight(purple[5])).toEqual(false);
+});
+
+test('Determine the darkness of a color', () => {
+  expect(isDark(yellow[5])).toEqual(false);
+  expect(isDark(purple[5])).toEqual(true);
 });
 
 test('should contain preseted palettes', () => {
