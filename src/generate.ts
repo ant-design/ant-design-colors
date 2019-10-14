4186aec214f7e97f067d92c0cffe76b58118aa11
rioarray <rio.array@windowslive.com>
2019-10-14 17:11:31 +0700

    added type data in some variable

--- src/generate.ts
+++ src/generate.ts
@@ -1,12 +1,12 @@
 import tinycolor from 'tinycolor2';
 
-const hueStep = 2; // 色相阶梯
-const saturationStep = 16; // 饱和度阶梯，浅色部分
-const saturationStep2 = 5; // 饱和度阶梯，深色部分
-const brightnessStep1 = 5; // 亮度阶梯，浅色部分
-const brightnessStep2 = 15; // 亮度阶梯，深色部分
-const lightColorCount = 5; // 浅色数量，主色上
-const darkColorCount = 4; // 深色数量，主色下
+const hueStep: number = 2; // 色相阶梯
+const saturationStep: number = 16; // 饱和度阶梯，浅色部分
+const saturationStep2: number = 5; // 饱和度阶梯，深色部分
+const brightnessStep1: number = 5; // 亮度阶梯，浅色部分
+const brightnessStep2: number = 15; // 亮度阶梯，深色部分
+const lightColorCount: number = 5; // 浅色数量，主色上
+const darkColorCount: number = 4; // 深色数量，主色下
 
 interface HsvObject {
   h: number;
@@ -65,11 +65,11 @@ function getValue(hsv: HsvObject, i: number, light?: boolean): number {
 }
 
 export default function generate(color: string): string[] {
-  const patterns = [] as string[];
+  const patterns: Array<string> = [];
   const pColor = tinycolor(color);
   for (let i = lightColorCount; i > 0; i -= 1) {
     const hsv = pColor.toHsv();
-    const colorString = tinycolor({
+    const colorString: string = tinycolor({
       h: getHue(hsv, i, true),
       s: getSaturation(hsv, i, true),
       v: getValue(hsv, i, true),
@@ -79,7 +79,7 @@ export default function generate(color: string): string[] {
   patterns.push(pColor.toHexString());
   for (let i = 1; i <= darkColorCount; i += 1) {
     const hsv = pColor.toHsv();
-    const colorString = tinycolor({
+    const colorString: string = tinycolor({
       h: getHue(hsv, i),
       s: getSaturation(hsv, i),
       v: getValue(hsv, i),
